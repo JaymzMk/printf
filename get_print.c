f80cddf070ee1e4699683c5c1a15cd79cc77f379
JaymzMk <Jamesmwaikmt@gmail.com>
2022-07-15 16:46:30 -0700

    Collabo Project

--- get_print.c
+++ get_print.c
@@ -5,30 +5,35 @@
  * depending on the conversion specifier passed to _printf
  * @s: character that holds the conversion specifier
  * Description: the function loops through the structs array
- * specs[] to find a match between the specifier passed to _printf
+ * func_arr[] to find a match between the specifier passed to _printf
  * and the first element of the struct, and then the approriate
  * printing function
  * Return: a pointer to the matching printing function
  */
-
-int (*get_print(char s))(va_list)
+int (*get_print(char s))(va_list, flags_t *)
 {
-fmt specs[] = {
-{'c', print_char},
-{'s', print_string},
-{'%', print_percent},
-{'i', print_int},
-{'d', print_int}
-};
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
 
-int ops = 5;
-int i;
+	register int i;
 
-for (i = 0; i < ops ; i++)
-{
-if (specs[i].sp == s)
-return (specs[i].f);
-}
-
-return (NULL);
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
 }
